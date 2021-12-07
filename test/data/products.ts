850679471c64732631a3657129da7f073e1e62a9
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2021-12-07 11:33:06 -0600

    Version 0.0.1

---
+++ test/data/products.ts
@@ -0,0 +1,10 @@
+export const product1 = {
+    id: '1',
+    title: 'Product 1 - Sin imagen',
+}
+
+export const product2 = {
+    id: '2',
+    title: 'Product 2 - Con imagen',
+    img: './coffee-mug.png',
+}
